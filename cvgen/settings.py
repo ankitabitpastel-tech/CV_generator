f36b8acae54509dc1113cf5352f4921f684f0d44
Ankita Dey <ankitadeybeporoya@Ankitas-MacBook-Air.local>
2025-11-14 13:50:07 +0530

    updates

--- cvgen/settings.py
+++ cvgen/settings.py
@@ -162,8 +162,8 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key-for-render')
-DEBUG = config('DEBUG', default=False, cast=bool)
-
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
